7d158d9a4766779525f23ca4730b168f1d7949fd
Barbados Clemens <caleb.ukle@yahoo.com>
2019-09-03 16:17:56 -0500

    add favicon fix code block

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,7 +112,7 @@ module.exports = {
         background_color: `#204969`,
         theme_color: `#204969`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `static/img/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // {
