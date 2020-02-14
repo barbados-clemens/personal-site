57b9173de38c8e6b63ff3b47ec9db99fae5be7ac
Caleb Ukle <caleb.ukle@yahoo.com>
2020-02-13 21:45:22 -0600

    playing with blog blockquote/code placement

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -1,14 +1,14 @@
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {ScullyLibModule} from '@scullyio/ng-lib';
-import {BlogRoutingModule} from './blog-routing.module';
-import {BlogComponent} from './blog.component';
-import {ShareSheetComponent} from './components/share-sheet/share-sheet.component';
-import {HeaderComponent} from './components/header/header.component';
-import {TagModule} from '../tag/tag.module';
-import {BlogOverviewComponent} from './components/blog-overview/blog-overview.component';
-import {SearchComponent} from './components/search/search.component';
-import {CardModule} from '../card/card.module';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ScullyLibModule } from '@scullyio/ng-lib';
+import { BlogRoutingModule } from './blog-routing.module';
+import { BlogComponent } from './blog.component';
+import { ShareSheetComponent } from './components/share-sheet/share-sheet.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TagModule } from '../tag/tag.module';
+import { BlogOverviewComponent } from './components/blog-overview/blog-overview.component';
+import { SearchComponent } from './components/search/search.component';
+import { CardModule } from '../card/card.module';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
