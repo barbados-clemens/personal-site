3005d66217708c232057fc6295e2addc6cdd2025
Caleb Ukle <caleb.ukle@yahoo.com>
2020-04-01 18:35:07 -0500

    reset the stage for tests

--- src/app/blog/components/blog-overview/blog-overview.component.ts
+++ src/app/blog/components/blog-overview/blog-overview.component.ts
@@ -3,6 +3,7 @@ import {ScullyRoutesService} from '@scullyio/ng-lib';
 import {map} from 'rxjs/operators';
 import {ActivatedRoute} from '@angular/router';
 import {Observable} from 'rxjs';
+import {DateHelperService} from '../../../services/date-helper.service';
 
 @Component({
   selector: 'app-blog-overview',
@@ -18,12 +19,12 @@ export class BlogOverviewComponent {
       map(posts => posts.sort((a, b) => new Date(a.date) > new Date(b.date) ? -1 : 1)),
       map(links => links.filter(l => l.route.startsWith('/blog/')).slice(0, 10)),
       map(posts => {
-        const dates = new Set(posts.map(p => this.parseDateToTitle(p.date)));
+        const dates = new Set(posts.map(p => this.dateSrv.parseDateToTitle(p.date)));
         return Array.from(dates)
           .map(d => {
             return {
               date: d,
-              posts: posts.filter(p => this.parseDateToTitle(p.date) === d)
+              posts: posts.filter(p => this.dateSrv.parseDateToTitle(p.date) === d)
             };
           });
       })
@@ -32,14 +33,7 @@ export class BlogOverviewComponent {
   constructor(
     private route: ActivatedRoute,
     private scully: ScullyRoutesService,
+    private dateSrv: DateHelperService,
   ) {
-  }
-
-  private parseDateToTitle(date: string) {
-    return new Intl.DateTimeFormat(
-      navigator.language,
-      {month: 'long', year: 'numeric'}
-    )
-      .format(new Date(date));
   }
 }
