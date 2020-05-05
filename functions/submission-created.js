1e8f35675ea9f7bafdb9bb092c8b0d6a5768ff04
Caleb Ukle <caleb.ukle@yahoo.com>
2020-05-05 09:27:47 -0500

    basic structure

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -6,16 +6,40 @@
  * @returns {Promise<void>}
  */
 exports.handler = async function (event, context, callback) {
+  const headers = {
+    'Content-Type': 'application/json'
+  }
 
-  console.log(JSON.stringify(context, null, 2), JSON.stringify(event, null, 2))
+  if (event.httpMethod !== 'POST' || !event.body) {
+    callback(null, {
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({error: 'Invalid request', method: event.httpMethod})
+    })
+  }
 
+  const {payload} = JSON.parse(event.body);
+
+  let res = {};
+  let statusCode = 200;
+
+  switch (payload.form_name) {
+    case 'contact':
+      console.log('Contact form submission detected. Doing nothing for now.')
+      res = {msg: 'new contact submission'}
+      break;
+
+    case 'newsletter':
+      console.log('Newsletter form submission detected. Adding to email list')
+      res = {msg: `TODO add ${payload.data.email} to email list`}
+      break;
+    default:
+      console.log(`unknown form name ${payload.form_name}`)
+  }
 
   callback(null, {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({})
+    statusCode,
+    headers,
+    body: JSON.stringify(res)
   })
-
 }
