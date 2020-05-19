a7589eb2737ca7d33224b81ec49a39d7fc346035
Caleb Ukle <caleb.ukle@gmail.com>
2020-05-19 16:47:10 +0000

    add canonical urls

--- src/app/layout/services/metadata/metadata.service.ts
+++ src/app/layout/services/metadata/metadata.service.ts
@@ -47,6 +47,10 @@ export class MetadataService {
     this.meta.updateTag({
       property: 'og:image', content: metadata.image || 'https://media.calebukle.com/uploads/icon-48x48.png'
     });
+
+    this.meta.updateTag({
+      property: 'canonical', href: metadata.url
+    })
   }
 }
 
