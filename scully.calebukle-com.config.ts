bc027b667329369e2ad818e96fcf01b7a2bb4b81
Caleb Ukle <caleb.ukle@yahoo.com>
2020-07-27 16:20:03 -0500

    what is life??

--- scully.calebukle-com.config.ts
+++ scully.calebukle-com.config.ts
@@ -1,6 +1,38 @@
-import {ScullyConfig, setPluginConfig} from '@scullyio/scully';
+require('dotenv').config()
+import {log, ScullyConfig, setPluginConfig, yellow} from '@scullyio/scully';
+import {AddLinksToHeader, HeaderLinksDefaultConfig} from '@barbados-clemens/scully-plugin-header-links'
+import {AddPostToFirebase, IFirebasePluginSettings} from '@barbados-clemens/scully-plugin-firebase-likes';
+
 
 setPluginConfig('md', {enableSyntaxHighlighting: true});
+setPluginConfig(AddLinksToHeader, HeaderLinksDefaultConfig)
+const firebaseConfig: IFirebasePluginSettings = {
+  databaseUrl: 'https://portfolio-82e83.firebaseio.com',
+  serviceAccount: JSON.parse(process.env.FIREBASE_CONFIG),
+  dryRun: false,
+  debug: true,
+}
+console.log(firebaseConfig)
+setPluginConfig(AddPostToFirebase, firebaseConfig)
+
+const blogPostRenderers = [
+  AddLinksToHeader,
+  AddPostToFirebase,
+]
+
+// if (process.env.NODE_ENV.toLowerCase() === 'production') {
+//   const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG)
+//   setPluginConfig(AddPostaddToFireBase, {
+//     credential: serviceAccount,
+//     databaseURL: "https://portfolio-82e83.firebaseio.com",
+//   })
+//   // blogPostRenderers.push(AddPostToFireBase)
+//   log(yellow(`Running production plugins`))
+// } else {
+//   log(yellow(`Not running in production, ${process.env.NODE_ENV || 'NODE_ENV not set'}`))
+// }
+
+
 export const config: ScullyConfig = {
   projectRoot: './src',
   projectName: 'calebukle-com',
@@ -8,6 +40,7 @@ export const config: ScullyConfig = {
   routes: {
     '/blog/:slug': {
       type: 'contentFolder',
+      postRenderers: [...blogPostRenderers],
       slug: {
         folder: './blog'
       }
